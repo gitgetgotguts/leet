6c273a315f9a92163400305ce80370a946360bc5
gitgetgotguts <getgetgit@proton.me>
2024-09-08 17:47:54 +0100

    nvm not solved time limit exceeded

--- anagram group.py
+++ anagram group.py
@@ -2,15 +2,41 @@ from collections import Counter
 
 strs =["eat","tea","tan","ate","nat","bat"]
 class Solution:
+
+
+
+
     def groupAnagrams(self, strs) :
         Clist = list(map(Counter, strs))
         # lists are unhashable so we use tuples
         # counters are unhashable also
+        Fstrs=strs.copy()
 
-        Hash = {}
-        for i,n in enumerate(strs):
-            if Clist[i] in Hash.keys():
-                Hash[Clist[i]].append(n)
-        else:Hash[Clist[i]] = n
-        return Hash.values()
+        Flist=[]
+        for i,str in enumerate(strs):
+            if str in Fstrs:
+                temp=[]
+                for j in range(len(Clist)):
+                    if Clist[i]==Clist[j]:
+                        temp.append(strs[j])
+                        Fstrs.remove(strs[j])
+
+
+                Flist.append(temp)
+
+
+        return Flist
+
+
+
+
+
+
+
+
+
+
+
+
+
 print(Solution().groupAnagrams(strs))
