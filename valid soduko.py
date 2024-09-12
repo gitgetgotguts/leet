ad717d5fd3626155359969c3fc5e1aaa50864dae
gitgetgotguts <getgetgit@proton.me>
2024-09-12 22:47:54 +0100

    solved valid soduko.py 36.

--- valid soduko.py
+++ valid soduko.py
@@ -1,35 +1,49 @@
-x=[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 import numpy as np
 from collections import Counter
 
 
-
-
-
 def Valid_Soduko_Square(a, x, y):
-    s={}
-    k=len(s)
-    for i in range(x*3, x*3 + 3):
-        for j in range(x*3, x*3 + 3):
-            if a[i][j] == '.': continue
+    s = set()
+    k = len(s)
+    for i in range(x * 3, x * 3 + 3):
+        for j in range(y * 3, y * 3 + 3):
+            if a[i][j] == '.':
+                continue
             else:
-                s.append(a[i][j])
-                if len(s) > k: k+= 1
-                else: return False
+                s.add(a[i][j])
+                if len(s) > k:
+                    k += 1
+                else:
+                    return False
     return True
 
-ret=[set(i.values()) for i in D]
 
 def check_lines(L):
-    D=set()
+    D = set()
     for i in L:
-        j=Counter(i)
+        j = Counter(i)
         if '.' in j:
             j.pop('.')
-        D.add([set(i.values()) for i in D])
-    if D!={{1}}:
-        return False
+        for k in j.values():
+            if any(v > 1 for v in j.values()):
+                return False
+    return True
 
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        board = np.array(board)
+        T = np.transpose(board)
+        a = check_lines(T)
+        b = check_lines(board)
+        c = 1
+        for i in range(3):
+            for j in range(3):
+                c = c and Valid_Soduko_Square(board, i, j)
+
+        return a and b and c
 
 
+# returning false in the second function wont break from the main one
+# solved had to add return True at the end to use binary logic
